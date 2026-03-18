8feb8b0c26cab931f102ad6063f2b842569baa2b
jocelino-fgv <jocelino.santos@fgv.edu.br>
2026-03-18 17:47:34 -0300

    Update pratica01_estrutura_dados.py

--- pratica01_estrutura_dados.py
+++ pratica01_estrutura_dados.py
@@ -1,71 +1,24 @@
-# class Node:
-#     def __init__(self, valor ):
-#         self.valor = valor
-#         self.next = None
+# def mostrar_menu():
+#     print("\n--- MENU ---")
+#     print("1. Cadastrar")
+#     print("2. Visualizar")
+#     print("3. Sair")
 
+# while True:
+#     mostrar_menu()
+#     opcao = input("Escolha uma opção (1-3): ")
 
-# class Encadeamento:
-#     def  __init__(self):
-#         self.head = None
-
-#     def insert_begin(self, valor):
-#         new_node = Node(valor)
-#         new_node.next = self.head
-#         self.head = new_node
-
-#     def insert_end(self, valor):
-#         new_node = Node(valor)
-#         if self.head is None:
-#             self.head = new_node
-#             return
-
-#         atual = self.head
-
-#         while atual.next:
-#            atual = atual.next
-#         atual.next = new_node
-
-#     def trocar_ultimo_primeiro(self):
-
-#         if self.head is None or self.head.next is None:
-#             return
-
-#         prev = None
-#         atual = self.head
-
-#         while atual.next:
-#             prev = atual
-#             atual = atual.next
-
-#         # atual = último
-#         # prev = penúltimo
-
-#         prev.next = self.head
-#         atual.next = self.head.next
-#         self.head.next = None
-#         self.head = atual
-
-#     def imprimir(self):
-#             atual = self.head
-#             while atual:
-#                 print(atual.valor, end=" ->")
-#                 atual = atual.next
-#             print("None")
-
-
-
-# lista = Encadeamento()
-# lista.insert_begin(10)
-# lista.insert_begin(7)
-# lista.insert_begin(23)
-# lista.insert_end(45)
-# lista.trocar_ultimo_primeiro()
-# lista.imprimir()
-# lista.trocar_ultimo_primeiro()
-# lista.imprimir()
-# lista.trocar_ultimo_primeiro()
-# lista.imprimir() 
-
+#     if opcao == '1':
+#         print("Opção 1: Cadastrando...")
+#         # Lógica de cadastro aqui
+#     elif opcao == '2':
+#         print("Opção 2: Visualizando...")
+#         # Lógica de visualização aqui
+#     elif opcao == '3':
+#         print("Saindo... Até logo!")
+#         break # Encerra o loop, fechando o programa
+#     else:
+#         print("Opção inválida! Tente novamente.")
 
 
 class Dado:
