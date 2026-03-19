56f965bdd4f2396882830ccca3af39335a6e4716
Jocelino Santos <jocelino.santos@fgv.edu.br>
2026-03-18 21:31:13 -0300

    20250318_21:31

--- pratica01_estrutura_dados.py
+++ pratica01_estrutura_dados.py
@@ -1,55 +1,27 @@
-# def mostrar_menu():
-#     print("\n--- MENU ---")
-#     print("1. Cadastrar")
-#     print("2. Visualizar")
-#     print("3. Sair")
-
-# while True:
-#     mostrar_menu()
-#     opcao = input("Escolha uma opção (1-3): ")
-
-#     if opcao == '1':
-#         print("Opção 1: Cadastrando...")
-#         # Lógica de cadastro aqui
-#     elif opcao == '2':
-#         print("Opção 2: Visualizando...")
-#         # Lógica de visualização aqui
-#     elif opcao == '3':
-#         print("Saindo... Até logo!")
-#         break # Encerra o loop, fechando o programa
-#     else:
-#         print("Opção inválida! Tente novamente.")
-
-
 class Dado:
     def __init__(self, valor):
         self.valor = valor
         self.next = None
         self.prev = None
-        
+
 
 class Elos:
     def __init__(self):
         self.head = None
-        
+
     def insert_end(self, valor):
         new_node = Dado(valor)
-        
         if self.head is None:
             self.head = new_node
             return
-       
         atual = self.head
         while atual.next:
             atual = atual.next
-            
         atual.next = new_node
         new_node.prev = atual
- 
-    def buscar_viz(self):
-        nome = "Pedro"
-        sv = "sem vizinho"
 
+    def buscar_viz(self):
+        sv = "sem vizinho"
         atual = self.head
         while atual:
             if atual.valor != nome:
@@ -69,30 +41,65 @@ class Elos:
                 return
         print("Nome não localizado")
 
+    # def insert_begin(self, valor):
+    #     new_node = Node(valor)
+    #     new_node.next = self.head
+    #     self.head = new_node
+    #
+    # def insert_end(self, valor):
+    #     new_node = Node(valor)
+    #     if self.head is None:
+    #         self.head = new_node
+    #         return
 
     def imprimir(self):
             atual = self.head
             while atual:
                 if atual.prev:
                     atual_prev = atual.prev.valor
-                else: 
+                else:
                     atual_prev = None
                 if atual.next:
                     atual_next = atual.next.valor
-                else: 
-                    atual_next = None                    
+                else:
+                    atual_next = None
 
                 print(atual_prev, atual.valor, atual_next)
                 atual = atual.next
 
-                
-                
+
+
 lista = Elos()
-lista.insert_end("João")
-lista.insert_end("Pedro")
-lista.insert_end("Maria")
-lista.insert_end("Carlos")
+# lista.insert_end("João")
+# lista.insert_end("Pedro")
+# lista.insert_end("Maria")
+# lista.insert_end("Carlos")
 lista.buscar_viz()
 #lista.imprimir()
-        
-                
+
+def mostrar_menu():
+    print("\n--- MENU ---")
+    print("(1) Inserir morador(a)")
+    print("(2) Buscar vizinhos de um dado morador")
+    print("(3) Ver moradores cujos vizinhos foram buscados")
+    print("(4) Finalizar execução")
+
+while True:
+    mostrar_menu()
+    opcao = input("Escolha uma opção (1-4): ")
+
+    if opcao == '1':
+        nome_morador = input("Cadastrar morador, informe o nome: ")
+        lista.insert_end(nome_morador)
+    elif opcao == '2':
+        nome = input("Informe o nome do morador ")
+        lista.buscar_viz()
+    elif opcao == '3':
+        hist_busca_moradores = input("Informe o nome do morador")
+    elif opcao == '4':
+        print("Encerrando, até breve.")
+        break
+    else:
+        print("Opção inválida! Tente novamente.")
+
+
